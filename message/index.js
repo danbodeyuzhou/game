6ffb1ca3f3d23be6bd00a7ddb3b6cb81594163b4
danbodeyuzhou <137911157+danbodeyuzhou@users.noreply.github.com>
2024-04-05 11:09:23 +0800

    Update and rename message/index.js to local.html

--- message/index.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var closeButton = document.getElementById("closeButton");
-  closeButton.addEventListener("click", function() {
-    // 重定向到about:blank，这看起来像是关闭了页面
-    window.location.href = "about:blank";
-  });
-});
